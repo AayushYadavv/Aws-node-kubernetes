83894762ff159d29ca5b0e336fd01b5e8c3ec072
Aayush Yadav <31856483+AayushYadavv@users.noreply.github.com>
2021-09-04 20:03:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) => {
-    res.send('<b>Hello there Aayush Here</b>')
+    res.send('<b>Hello there Samisksha</b>')
     })
     
     app.listen(3000)
